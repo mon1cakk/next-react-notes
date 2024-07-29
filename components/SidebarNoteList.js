7c9b1a9221166d390bdccb1155e0391a1c061744
mon1cakk <435421355@qq.com>
2024-07-29 23:04:36 +0800

    feat: 优化搜索框

--- components/SidebarNoteList.js
+++ components/SidebarNoteList.js
@@ -1,14 +1,31 @@
 import SidebarNoteListFilter from '@/components/SidebarNoteListFilter';
 import { getAllNotes } from '@/lib/redis';
 import { sleep } from '@/lib/utils';
+import SidebarNoteItemHeader from '@/components/SidebarNoteItemHeader';
 
 export default async function NoteList() {
-  await sleep(3000);
+  await sleep(2000);
   const notes = await getAllNotes();
 
   if (Object.entries(notes).length == 0) {
     return <div className='notes-empty'>{'No notes created yet!'}</div>;
   }
 
-  return <SidebarNoteListFilter notes={notes} />;
+  return (
+    <SidebarNoteListFilter
+      notes={Object.entries(notes).map(([noteId, note]) => {
+        const noteData = JSON.parse(note);
+        return {
+          noteId,
+          note: noteData,
+          header: (
+            <SidebarNoteItemHeader
+              title={noteData.title}
+              updateTime={noteData.updateTime}
+            />
+          ),
+        };
+      })}
+    />
+  );
 }
